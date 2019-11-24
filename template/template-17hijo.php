5a498bf469a08cb003df976bed2106cce65fdbc3
EC2 Default User <ec2-user@ip-172-31-19-171.eu-west-1.compute.internal>
2019-11-24 17:21:45 +0000

    Carpeta twentyseventeen-child

---
+++ template/template-17hijo.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Template Name: Full Width Page 17 CHILD
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
